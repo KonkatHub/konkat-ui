1d218e02cc999870021dfadff3154b1575e90b7f
Mathis Côté <mathis.cote1@outlook.com>
2023-06-25 18:46:16 -0400

    [1] feat: add basic design for Button

---
+++ src/lib/components/button/Button.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { ButtonPrimitive, type ButtonProps } from '@konkat/konkat-ui-primitives';
+
+  type $$Props = ButtonProps;
+</script>
+
+<ButtonPrimitive
+  class="rounded bg-purple-800 px-4 py-2 text-purple-100 transition-all hover:bg-purple-900 active:enabled:scale-95 disabled:cursor-not-allowed disabled:bg-purple-800/50 disabled:text-purple-100/50"
+  {...$$restProps}
+  on:click
+  on:change
+  on:keydown
+  on:keyup
+  on:touchstart
+  on:touchend
+  on:touchcancel
+  on:mouseenter
+  on:mouseleave
+>
+  <slot />
+</ButtonPrimitive>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,6 @@
   import '../app.postcss';
 </script>
 
-<slot />
+<main class="flex h-screen w-screen items-center justify-center bg-slate-800 text-slate-200">
+  <slot />
+</main>
