a44dbd701e20fdb8294b634453de0f3d0d77a998
Mathis Côté <mathis.cote1@outlook.com>
2023-07-06 21:30:37 -0400

    [5] fix: update UI inconsistencies

--- src/lib/components/checkbox/index.ts
+++ src/lib/components/checkbox/index.ts
@@ -3,29 +3,43 @@ import { cva } from 'class-variance-authority';
 export { default as Checkbox } from './Checkbox.svelte';
 
 export const boxVariants = cva(
-  ['flex appearance-none items-center justify-center rounded border transition-[background-color]'],
+  [
+    'flex appearance-none items-center justify-center ring-offset-slate-300 rounded border transition-[background-color]',
+    'focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2',
+    'data-[state=checked]:text-white',
+    'data-[state=indeterminate]:text-white',
+    'disabled:cursor-not-allowed',
+  ],
   {
     variants: {
       theme: {
         primary: [
-          'border-primary-700 text-white',
+          'border-primary-700',
           'data-[state=checked]:bg-primary-700 data-[state=checked]:border-primary-700',
-          'disabled:bg-primary-200 disabled:data-[state=checked]:bg-primary-200 disabled:data-[state=checked]:border-primary-200 disabled:border-primary-200 disabled:text-primary-500 disabled:cursor-not-allowed disabled:opacity-50',
+          'data-[state=indeterminate]:bg-primary-700 data-[state=indeterminate]:border-primary-700',
+          'focus-visible:outline-primary-700',
+          'disabled:bg-primary-200 disabled:data-[state=checked]:bg-primary-200 disabled:data-[state=checked]:border-primary-200 disabled:border-primary-200 disabled:text-primary-500 ',
         ],
         secondary: [
-          'border-secondary text-white',
+          'border-secondary',
           'data-[state=checked]:bg-secondary data-[state=checked]:border-secondary',
-          'disabled:bg-secondary-200 disabled:data-[state=checked]:bg-secondary-200 disabled:data-[state=checked]:border-secondary-200 disabled:border-secondary-200 disabled:text-secondary-500 disabled:cursor-not-allowed disabled:opacity-50',
+          'data-[state=indeterminate]:bg-secondary data-[state=indeterminate]:border-secondary',
+          'focus-visible:outline-secondary',
+          'disabled:bg-secondary-200 disabled:data-[state=checked]:bg-secondary-200 disabled:data-[state=checked]:border-secondary-200 disabled:border-secondary-200 disabled:text-secondary-500',
         ],
         accent: [
-          'border-accent text-white',
+          'border-accent',
           'data-[state=checked]:bg-accent data-[state=checked]:border-accent',
-          'disabled:bg-accent-200 disabled:data-[state=checked]:bg-accent-200 disabled:data-[state=checked]:border-accent-200 disabled:border-accent-200 disabled:text-accent-500 disabled:cursor-not-allowed disabled:opacity-50',
+          'data-[state=indeterminate]:bg-accent data-[state=indeterminate]:border-accent',
+          'focus-visible:outline-accent',
+          'disabled:bg-accent-200 disabled:data-[state=checked]:bg-accent-200 disabled:data-[state=checked]:border-accent-200 disabled:border-accent-200 disabled:text-accent-500',
         ],
         destructive: [
-          'border-destructive text-white',
+          'border-destructive',
           'data-[state=checked]:bg-destructive data-[state=checked]:border-destructive',
-          'disabled:bg-destructive-200 disabled:data-[state=checked]:bg-destructive-200 disabled:data-[state=checked]:border-destructive-200 disabled:border-destructive-200 disabled:text-destructive-500 disabled:cursor-not-allowed disabled:opacity-50',
+          'data-[state=indeterminate]:bg-destructive data-[state=indeterminate]:border-destructive',
+          'focus-visible:outline-destructive',
+          'disabled:bg-destructive-200 disabled:data-[state=checked]:bg-destructive-200 disabled:data-[state=checked]:border-destructive-200 disabled:border-destructive-200 disabled:text-destructive-500',
         ],
       },
       size: {
