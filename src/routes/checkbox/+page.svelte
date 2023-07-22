1d32ce097e4fc1b46b13bae5c4eaf3ce91639b24
Mathis Côté <mathis.cote1@outlook.com>
2023-07-22 11:38:45 -0400

    [4] test: staging github action

--- src/routes/checkbox/+page.svelte
+++ src/routes/checkbox/+page.svelte
@@ -1,36 +1,8 @@
 <script lang="ts">
   import Article from '$components/Article.svelte';
-  import Table from '$components/TableOfContent/Table.svelte';
   import type { LinkInfo } from '$components/TableOfContent/Link.svelte';
 
-  const links: LinkInfo[] = [
-    {
-      href: '#theme',
-      text: 'Theme',
-      child: [
-        {
-          href: '#primary',
-          text: 'Primary',
-        },
-        {
-          href: '#secondary',
-          text: 'Secondary',
-        },
-        {
-          href: '#accent',
-          text: 'Accent',
-        },
-        {
-          href: '#destructive',
-          text: 'Destructive',
-        },
-      ],
-    },
-    {
-      href: '#size',
-      text: 'Size',
-    },
-  ];
+  const links: LinkInfo[] = [];
 </script>
 
 <Article title="Checkbox" {links} />
