1f8ba4a688d6b0be3b40fd610c1f35defd7c6eda
Mathis Côté <mathis.cote1@outlook.com>
2023-07-23 14:20:51 -0400

    [11] feat: add CodeInline component

---
+++ src/components/CodeInline.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { cn } from '$lib/utils';
+
+  let className: string | undefined | null = undefined;
+  export { className as class };
+</script>
+
+<code class={cn('rounded-md bg-base-400 px-2 py-1 text-sm', className)}>
+  <slot />
+</code>
--- src/routes/(doc)/(getting-started)/installation/+page.svelte
+++ src/routes/(doc)/(getting-started)/installation/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Article from '$components/Article.svelte';
+  import CodeInline from '$components/CodeInline.svelte';
   import Codeblock from '$components/Codeblock.svelte';
   import DocSection from '$components/DocSection.svelte';
   import type { LinkInfo } from '$components/TableOfContent/Link.svelte';
@@ -30,7 +31,8 @@
 <Article title="Installation" {links}>
   <DocSection title="SvelteKit" id="sveltekit">
     <svelte:fragment slot="description">
-      You will need a SvelteKit project to use this library. If you don't have one, you can
+      You will need a <CodeInline>SvelteKit</CodeInline> project to use this library. If you don't have
+      one, you can
       <Link blank href="https://kit.svelte.dev/docs/creating-a-project">create a new project</Link> by
       running the following command:
     </svelte:fragment>
@@ -39,21 +41,26 @@
   <DocSection title="Dependencies" id="dependencies">
     <svelte:fragment slot="description">
       To use this library, you will need
-      <Link href="https://tailwindcss.com/docs/installation" blank>TailwindCSS</Link> and
-      <Link href="https://www.melt-ui.com/docs/installation" blank>MeltUI</Link> installed in your project.
+      <CodeInline>
+        <Link href="https://tailwindcss.com/docs/installation" blank>TailwindCSS</Link>
+      </CodeInline> and
+      <CodeInline>
+        <Link href="https://www.melt-ui.com/docs/installation" blank>MeltUI</Link>
+      </CodeInline>
+      installed in your project.
     </svelte:fragment>
     <Codeblock code={data.dependencies} language="bash" />
   </DocSection>
   <DocSection title="Installation" id="installation">
     <svelte:fragment slot="description">
-      To install Konkat/UI, run the following command:
+      To install <CodeInline>Konkat/UI</CodeInline>, run the following command:
     </svelte:fragment>
     <Codeblock code={data.install} language="bash" />
   </DocSection>
   <DocSection title="CSS" id="css">
     <svelte:fragment slot="description">
-      You will need to add CSS variables to your project. You can do this by adding the following
-      line to your "app.postcss" file:
+      You will need to add <CodeInline>CSS</CodeInline> variables to your project. You can do this by
+      adding the following line to your <CodeInline>app.postcss</CodeInline> file:
     </svelte:fragment>
     <Codeblock code={data.css} language="css" />
   </DocSection>
