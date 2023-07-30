aa4224d91a8efc0b592e6ef9abe8da96495fecb2
BenocxX <mathis.cote@outlook.com>
2023-07-30 14:25:56 -0400

    [29] fix: plugin import

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,3 @@
-export { konkatPlugin } from './konkatPlugin.js';
-
 export { Button } from './components/button/index.js';
 export { Link } from './components/link/index.js';
 export { Checkbox } from './components/checkbox/index.js';
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,5 @@ import { konkatPlugin } from './src/lib/konkatPlugin';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
   plugins: [konkatPlugin],
 };
